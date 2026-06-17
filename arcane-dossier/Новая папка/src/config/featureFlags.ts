7f17d5d980a3bcf008b8f4c8be9d92927f6401e5
Yessimbek Galymzhan <esm.2001@mail.ru>
2026-06-17 16:00:23 +0500

    delete trash files

--- arcane-dossier/Новая папка/src/config/featureFlags.ts
+++
@@ -1,8 +0,0 @@
-export const featureFlags = {
-  enableThreeHero: true,
-  enableMusicMap: true,
-  enableStaticOracle: true,
-  enableOllamaProvider: false,
-  enableBrowserLLMProvider: false,
-  enableCloudProvider: false,
-} as const;
